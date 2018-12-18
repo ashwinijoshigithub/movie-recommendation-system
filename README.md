76674ad8dd7925c729af458481dc602c39c24169
ashwinijoshigithub <ashu.94joshi@gmail.com>
2018-12-18 15:50:09 +0530

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # movie-recommendation-system
-Recommend movies to users based on different collaborative filtering approaches
+
+Recommend movies to users based on different collaborative filtering approaches.
+
+This project used MovieLens - 100K dataset for predictions.
+Link to dataset: http://grouplens.org/datasets/movielens/100k/
+Information about the dataset can be found here: http://files.grouplens.org/datasets/movielens/ml-100k-README.txt
+
+In order to run the code, the python file should be stored in the same folder where the dataset resides.
+Command for running code:
+>> python main.py - -data - -item
+
+--data expects path to u.data file in the movie - lens dataset.
+u.data: The full u data set, 100000 ratings by 943 users on 1682 items. his is a tab separated list of
+user id | item id | rating | timestamp
+
+--item expects path to u.item file in the movie - lens dataset.
+u.item: Information about the items(movies). This is a tab separated list of
+movie id | movie title | release date | video release date |
+IMDb URL | unknown | Action | Adventure | Animation |
+Children's | Comedy | Crime | Documentary | Drama | Fantasy |
+Film - Noir | Horror | Musical | Mystery | Romance | Sci - Fi |
+Thriller | War | Western |
+
+Four different output files are generated in the folder 'output_files':
+Item - based Cosine(cosine_predictions_item.csv)
+User - based Cosine(cosine_predictions_user.csv)
+SVD(svd_predictions.csv)
+
---
+++ main.py
@@ -0,0 +1,257 @@
+# import packages
+import csv
+import sklearn
+import argparse
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from pathlib import Path
+from sklearn import preprocessing
+from scipy.sparse.linalg import svds
+from sklearn.metrics import mean_squared_error
+
+
+def train_test_split(ratings):
+    '''
+    function to split data into test and train
+    :param ratings: Numpy array having complete data
+    :return: Numpy array of test and train data
+
+    '''
+    test = np.zeros(ratings.shape)
+    train = ratings.copy()
+    for user in range(ratings.shape[0]):
+        test_ratings = np.random.choice(
+            ratings[user, :].nonzero()[0], size=10, replace=False)
+        train[user, test_ratings] = 0.
+        test[user, test_ratings] = ratings[user, test_ratings]
+    assert(np.all((train * test) == 0))
+    return train, test
+
+
+def pairwise_distances_scratch(matrix):
+    '''
+    Function to calculate cosine similarity
+    :param matrix: Numpy array whose distances are to be
+        calculated
+    :return distances: Numpy array of calculated distances
+        using cosine similarity
+    '''
+    size = matrix.shape[0]
+    distances = np.empty(shape=(size, size), dtype=float)
+    for i in range(0, size):
+        norm_i = np.linalg.norm(matrix[i])
+        for j in range(0, size):
+            dot_product = np.dot(matrix[i], matrix[j])
+            norm_j = np.linalg.norm(matrix[j])
+            if norm_i == 0 or norm_j == 0:
+                distances[i][j] = 1
+            else:
+                distances[i][j] = 1 - (dot_product / (norm_i * norm_j))
+    return distances
+
+
+def predict_topk_nobias(ratings, similarity, kind='user', k=40):
+    '''
+    predict movies using top-k values and no bias
+
+    In case of user-based rating, certain users may tend to always give
+    high or low ratings to all movies. For example, one user might give 5
+    stars to all movies he likes and 1 to all he dislikes. Another user might
+    give 4 and 2 for the movies he likes and dislikes. To remove this bias,
+    mean rating is subtracted each because relative difference in the
+    ratings that these users give is more important than the absolute rating
+    values. In case of item-based filtering, this issue does not arise because
+    we consider all ratings for items from a single user.
+
+    :param ratings: Numpy array of train data (ratings)
+    :param similarity: Numpy array of similarity matrix calculated using pairwise
+        distances
+    :param kind: user/item based similarity
+    :param k: value of k for top k ratings
+    '''
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        user_bias = ratings.mean(axis=1)
+        ratings = (ratings - user_bias[:, np.newaxis]).copy()
+        for i in range(ratings.shape[0]):
+            top_k_users = [np.argsort(similarity[:, i])[:-k - 1:-1]]
+            for j in range(ratings.shape[1]):
+                pred[i, j] = similarity[i, :][top_k_users].dot(
+                    ratings[:, j][top_k_users])
+                pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
+        pred += user_bias[:, np.newaxis]
+    if kind == 'item':
+        for j in range(ratings.shape[1]):
+            top_k_items = [np.argsort(similarity[:, j])[:-k - 1:-1]]
+            for i in range(ratings.shape[0]):
+                pred[i, j] = similarity[j, :][top_k_items].dot(
+                    ratings[i, :][top_k_items].T)
+                pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items]))
+
+    return pred
+
+
+def get_rmse(pred, actual):
+    '''
+    Function to calculate RMSE between predicted and actual values
+    :param pred: Predicted ratings
+    :param actual: Actual ratings
+    :return rmse: RMSE of predicted and actual values
+    '''
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+
+    return np.sqrt(mean_squared_error(pred, actual))
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data',
+        type=str,
+        help='Path to u.data file',
+        required=True)
+    parser.add_argument(
+        '--item',
+        type=str,
+        help='Path to u.item file',
+        required=True)
+    args = parser.parse_args()
+
+    # reading CSV file
+    rating_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+    df_data = pd.read_csv(
+        'ml-100k/u.data',
+        sep='\t',
+        names=rating_cols,
+        encoding='latin-1')
+
+    # unique users and items
+    n_users = df_data.user_id.unique().shape[0]
+    n_items = df_data.movie_id.unique().shape[0]
+
+    # mapping movie id to movie name
+    df_item = pd.read_csv(
+        'ml-100k/u.item',
+        sep='|',
+        header=None,
+        usecols=[
+            0,
+            1],
+        encoding='latin-1')
+    df_movie = df_item.values
+
+    # converting data into user-movie matrix
+    ratings = np.zeros((n_users, n_items))
+    for row in df_data.itertuples():
+        ratings[row[1] - 1, row[2] - 1] = row[3]
+
+    # split into train and test
+    train_data_matrix, test_data_matrix = train_test_split(ratings)
+
+    # Cosine Similarity
+
+    # user-user
+    user_similarity = pairwise_distances_scratch(train_data_matrix)
+    # item-item
+    item_similarity = pairwise_distances_scratch(train_data_matrix.T,)
+
+    # prediction- both item and user using function defined
+    item_prediction = predict_topk_nobias(
+        train_data_matrix, item_similarity, kind='item', k=15)
+    user_prediction = predict_topk_nobias(
+        train_data_matrix, user_similarity, kind='user', k=50)
+
+    # Scaling the ratings of movies in range 1 to 5
+    # which were normalized while removing bias
+
+    # item
+    scaler = preprocessing.MinMaxScaler(feature_range=(1, 5))
+    item_prediction = scaler.fit_transform(item_prediction)
+    # user
+    scaler = preprocessing.MinMaxScaler(feature_range=(1, 5))
+    user_prediction = scaler.fit_transform(user_prediction)
+
+    # storing top 5 predictions for each user
+
+    # item
+    top_predictions_cosine = np.empty([n_users, 6], dtype=object)
+    for i in range(0, item_prediction.shape[0]):
+        index = item_prediction[i].argsort()[-5:][::-1]
+        top_predictions_cosine[i][0] = i + 1
+        k = 1
+        for j in index:
+            top_predictions_cosine[i][k] = df_movie[j][1]
+            k = k + 1
+
+    # user
+    top_predictions_cosine_user = np.empty([n_users, 6], dtype=object)
+    for i in range(0, user_prediction.shape[0]):
+        index = user_prediction[i].argsort()[-5:][::-1]
+        top_predictions_cosine_user[i][0] = i + 1
+        k = 1
+        for j in index:
+            top_predictions_cosine_user[i][k] = df_movie[j][1]
+            k = k + 1
+
+    # writing to csv file
+
+    # Create output directory
+    Path('./output_files').mkdir(parents=True, exist_ok=True)
+
+    heading_list = []
+    heading_list.append('User ID')
+    heading_list.append('Movie 1')
+    heading_list.append('Movie 2')
+    heading_list.append('Movie 3')
+    heading_list.append('Movie 4')
+    heading_list.append('Movie 5')
+
+    print('Item-based RMSE: ' + str(get_rmse(item_prediction, test_data_matrix)))
+    # item-item predictions
+    with open('./output_files/cosine_predictions_item.csv', 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(heading_list)
+        wr.writerows(top_predictions_cosine)
+
+    print('User-based RMSE: ' + str(get_rmse(user_prediction, test_data_matrix)))
+    # user-user predictions
+    with open('./output_files/cosine_predictions_user.csv', 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(heading_list)
+        wr.writerows(top_predictions_cosine_user)
+
+    # Singular Value Decomposition
+
+    # get SVD components from train matrix. Choose k.
+    U, sigma, vT = svds(train_data_matrix, k=19)
+    s_diag_matrix = np.diag(sigma)
+    # prediction using SVD
+    svd_pred = np.dot(np.dot(U, s_diag_matrix), vT)
+
+    # scaling the ratings in range 1 to 5
+    scaler = preprocessing.MinMaxScaler(feature_range=(1, 5))
+    svd_pred = scaler.fit_transform(svd_pred)
+
+    # storing top 5 movies for each user
+    top_predictions_svd = np.empty([n_users, 6], dtype=object)
+    for i in range(0, svd_pred.shape[0]):
+        index = svd_pred[i].argsort()[-5:][::-1]
+        top_predictions_svd[i][0] = i + 1
+        k = 1
+        for j in index:
+            top_predictions_svd[i][k] = df_movie[j][1]
+            k = k + 1
+
+    print('SVD RMSE: ' + str(get_rmse(svd_pred, test_data_matrix)))
+    # writing to csv file (SVD)
+    with open('./output_files/svd_predictions.csv', 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(heading_list)
+        wr.writerows(top_predictions_svd)
+
+
+if __name__ == '__main__':
+    main()
